fff18ccf95ce80525d24957bae78b252de0bce99
yeabsiraM290 <yeabsiraM@outlook.com>
2022-12-09 20:25:43 +0300

    test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ s = sched.scheduler(time.time, time.sleep)
 def ping(sc): 
     requests.get(url)
     print("Requst sent...")
-    sc.enter(1500, 1, ping, (sc,))
+    sc.enter(60, 1, ping, (sc,))
 
-s.enter(1500, 1, ping, (s,))
+s.enter(60, 1, ping, (s,))
 s.run() 
